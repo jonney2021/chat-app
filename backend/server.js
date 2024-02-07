60a48aa912cae9751055f07e39b1cb77f3c0c87c
Yeming Hu <yeminghuhym@gmail.com>
2024-02-07 13:19:11 -0500

    added backend logic for auth, message and user

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  //root route http://localhost:5000/
-  res.send("hello world!!!");
-});
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to parse the incoming cookies from req.cookes
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//   //root route http://localhost:5000/
+//   res.send("hello world!!!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
