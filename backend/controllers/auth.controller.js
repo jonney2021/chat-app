e966261873aa5610f139c4777eb301d4637c8677
Yeming Hu <yeminghuhym@gmail.com>
2024-02-06 23:11:38 -0500

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+  } catch (error) {}
+};
+
+export const login = (req, res) => {
+  res.send("loginUser");
+  console.log("loginUser");
+};
+
+export const logout = (req, res) => {
+  res.send("logoutUser");
+  console.log("logoutUser");
+};
